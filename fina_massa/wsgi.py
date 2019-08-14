16053607d4ae5ca1ac8aa7ea06ad527a4beaa3cc
José Davi Viana Francelino <jose.davi@academico.ifrn.edu.br>
2019-08-14 11:01:28 -0300

    Criei o projeto django

---
+++ fina_massa/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for fina_massa project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fina_massa.settings')
+
+application = get_wsgi_application()
