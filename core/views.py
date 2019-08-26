01a67f7a836bd361711cb208d97bde3b80fdcd32
Vivizinea <viviannyfelixvcfb@gmail.com>
2019-08-26 15:27:16 +0200

    Adicionei o cardápio e uma foto no título da página

--- core/views.py
+++ core/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 
 def index(request):
     return render(request, 'index.html', {'index': 'active'})
 
+def registro(request):
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('login')
+    contexto = {
+        'form': form,
+    }
+    return render(request, 'registration/registro.html', contexto)
+
+def perfil(request):
+	return render(request, 'registration/perfil.html')
 
 def sobre(request):
     return render(request, 'sobre.html', {'sobre': 'active'})
-
 
 def cardapio_cadastro(request):
     return render(request, 'cardapio_cadastro.html')
