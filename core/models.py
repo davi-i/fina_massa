7f8c23bc1b4dee724088dacc1f02990bca79e9f5
Lari <larissaandressaprojetos@gmail.com>
2019-09-02 07:49:21 -0300

    editei o arquivo models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class ItemCardapio(Models.model):
+	Tipo = models.Charfield('Tipo', max_length=100)
+	Preco = models.Charfield('Preco', FIELDNAME = forms.FloatField())
+	Ingredientes = models.Charfield('Ingredientes', max_length=100)
+	FiliaisD = models.Charfield('FiliaisD', max_length=100)
+
+		
