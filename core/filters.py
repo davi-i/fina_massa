5bde771e901f54f6a6b8affbdaf5764fcf69c773
davi-i <josedavifracelino@gmail.com>
2019-09-02 13:25:06 -0300

    Terminei o código de filtragem (falta intregar com cardápio)

--- core/filters.py
+++ core/filters.py
@@ -2,7 +2,16 @@ import django_filters
 from .models import ItemCardapio
 
 
-class ItemCardapioFilter(django_filters.Filter):
+class ItemCardapioFilter(django_filters.FilterSet):
+    preco = django_filters.NumberFilter(label="Preço", lookup_expr='exact')
+    preco_minimo = django_filters.NumberFilter(label="Preço mínimo",
+                                               field_name='preco',
+                                               lookup_expr='gt')
+    preco_maximo = django_filters.NumberFilter(label="Preço máximo",
+                                               field_name='preco',
+                                               lookup_expr='lt')
+    tipo = django_filters.CharFilter(label='Tipo', lookup_expr='icontains')
+
     class Meta:
         model = ItemCardapio
-        fields = ['tipo', 'preco', 'filial']
+        fields = ['filiais']
